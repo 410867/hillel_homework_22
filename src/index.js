3a380cb35ce8bf7c2d66ea3060e14eef631ce12a
410867 <oshchyp123@gmail.com>
2022-12-02 17:44:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@
  * @param {Object} object
  * @returns {string[]}
  */
-export const keys = (object) => {};
+export const keys = (object) => {
+  let keys = [];
+  for(let key in object) {
+    keys.push(key);
+  }
+  return  keys;
+};
 
 /**
  * Принимает объект, возвращает массив значений объекта. Также без использования
@@ -12,14 +18,26 @@ export const keys = (object) => {};
  * @param {Object} object
  * @returns {any[]}
  */
-export const values = (object) => {};
+export const values = (object) => {
+  let values = [];
+  for(let key in object) {
+    values.push(object[key]);
+  }
+  return  values;
+};
 
 /**
  * Принимаем объект, возвращаем энтри. Все также без методов, используем цикл
  * @param {Object} object
  * @returns {[string, any][]}
  */
-export const entries = (object) => {};
+export const entries = (object) => {
+  let entries = [];
+  for(let key in object) {
+    entries.push([key, object[key]]);
+  }
+  return entries;
+};
 
 /**
  * Тут наоборот, мы должны сделать из энтри объект обратно) Можете погуглить
@@ -27,4 +45,11 @@ export const entries = (object) => {};
  * @param {[string, any][]} entries
  * @returns {Object}
  */
-export const fromEntries = (entries) => {};
+export const fromEntries = (entries) => {
+  let fromEntries = {};
+  for (let item of entries) {
+    fromEntries[item[0]] = item[1];
+  }
+  //fromEntries = Object.fromEntries(entries);
+  return fromEntries;
+};
